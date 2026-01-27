6005571cf1fcca3a7b1d2db4cf49e722d1330c4d
professionalowo <martin.brandl1307@gmail.com>
2026-01-27 22:27:48 +0100

    fix possible memory leak

--- vm/vars.c
+++ vm/vars.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
@@ -5,8 +6,8 @@
 
 static struct varalloc varalloc;
 
-struct var *_var_lookup (char *id, struct var *list, int len, int block) {
-  for (int i = len-1; i >= 0; i--) {
+struct var *_var_lookup(char *id, struct var *list, int len, int block) {
+  for (int i = len - 1; i >= 0; i--) {
     if (strcmp(list[i].id, id) == 0)
       return &list[i];
     if (list[i].nr == VAR_BLOCKBORDER && block)
@@ -16,11 +17,11 @@ struct var *_var_lookup (char *id, struct var *list, int len, int block) {
   return NULL;
 }
 
-struct var *_var_add (char *id, struct var **list, int *len, int global) {
+struct var *_var_add(char *id, struct var **list, int *len, int global) {
   if (id && _var_lookup(id, *list, *len, 1))
     return NULL;
 
-  *list = realloc(*list, (*len+1) * sizeof(**list));
+  *list = realloc(*list, (*len + 1) * sizeof(**list));
   if (id) {
     (*list)[*len].id = strdup(id);
     (*list)[*len].nr = *len;
@@ -32,18 +33,18 @@ struct var *_var_add (char *id, struct var **list, int *len, int global) {
   (*list)[*len].global = global;
   (*len)++;
 
-  return &(*list)[*len-1];
+  return &(*list)[*len - 1];
 }
 
-struct var *var_add_global (char *id) {
+struct var *var_add_global(char *id) {
   return _var_add(id, &varalloc.globals, &varalloc.nrglobals, 1);
 }
 
-struct var *var_add_local (char *id) {
+struct var *var_add_local(char *id) {
   return _var_add(id, &varalloc.locals, &varalloc.nrlocals, 0);
 }
 
-struct var *var_get_or_addlocal (char *id) {
+struct var *var_get_or_addlocal(char *id) {
   struct var *ret = _var_lookup(id, varalloc.locals, varalloc.nrlocals, 0);
   if (!ret)
     ret = _var_lookup(id, varalloc.globals, varalloc.nrglobals, 0);
@@ -53,7 +54,7 @@ struct var *var_get_or_addlocal (char *id) {
   return ret;
 }
 
-struct var *var_get (char *id) {
+struct var *var_get(char *id) {
   struct var *ret = _var_lookup(id, varalloc.locals, varalloc.nrlocals, 0);
   if (!ret)
     ret = _var_lookup(id, varalloc.globals, varalloc.nrglobals, 0);
@@ -61,12 +62,15 @@ struct var *var_get (char *id) {
   return ret;
 }
 
-void var_enter_block (void) {
-  var_add_local(NULL);
-}
+void var_enter_block(void) { var_add_local(NULL); }
 
-void var_leave_block (void) {
-  for (int i = varalloc.nrlocals-1; i >= 0; i--) {
+void var_leave_block(void) {
+  for (int i = varalloc.nrlocals - 1; i >= 0; i--) {
+    if (varalloc.locals[i].id) {
+      // TODO: verify this is a real fix
+      free(varalloc.locals[i].id);
+    }
+
     if (varalloc.locals[i].nr == VAR_BLOCKBORDER) {
       varalloc.nrlocals = i;
       break;
@@ -74,13 +78,9 @@ void var_leave_block (void) {
   }
 }
 
-void var_reset (void) {
-  varalloc.nrlocals = 0;
-}
+void var_reset(void) { varalloc.nrlocals = 0; }
 
-void var_reset_all (void) {
+void var_reset_all(void) {
   varalloc.nrlocals = 0;
   varalloc.nrglobals = 0;
 }
-
-
